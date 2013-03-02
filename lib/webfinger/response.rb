ac0ed5c2fe45ecb6bda99acf7310bc36454ff20e
nov <nov@matake.jp>
2013-03-02 16:28:00 +0900

    use ActiveSupport::HashWithIndifferentAccess instead of Hash
    because I stopped using symbolize_keys option, I needed this change also.

--- lib/webfinger/response.rb
+++ lib/webfinger/response.rb
@@ -2,7 +2,7 @@
 #  Make a JSON Resource Descriptor (JRD) gem as separate one and use it as superclass?
 
 module WebFinger
-  class Response < Hash
+  class Response < ActiveSupport::HashWithIndifferentAccess
     def initialize(jrd)
       replace jrd
     end
--- spec/webfinger/response_spec.rb
+++ spec/webfinger/response_spec.rb
@@ -10,15 +10,18 @@ describe WebFinger::Response do
     [{
       rel: 'http://openid.net/specs/connect/1.0/issuer',
       href: 'https://openid.example.com/'
-    }]
+    }.with_indifferent_access]
   end
-  subject do
-    WebFinger::Response.new(
+  let(:attributes) do
+    {
       subject: _subject_,
       aliases: aliases,
       properties: properties,
       links: links
-    )
+    }.with_indifferent_access
+  end
+  subject do
+    WebFinger::Response.new attributes
   end
 
   its(:subject)    { should == _subject_ }
